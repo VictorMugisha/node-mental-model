805aae1d923a118fcd4d71db1072676b603d56ec
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-04 13:55:58 +0200

    chore: respond GET request with content from 'input.txt' file

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
+const fs = require("fs");
 const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+  if (method === "GET" && url === "/") {
+    const stream = fs.readFileSync("./input.txt");
+    res.end(stream);
+  }
+});
+
+
+server.listen(3000, function() {
+    console.log("Server is running on port 3000");
+})
