99a11ecfa1f3db7824ec4f1a7aed3000ed7e9e26
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-05 09:12:54 +0200

    Handling GET request

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const http = require("http");
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
   if (method === "GET" && url === "/") {
-    const stream = fs.readFileSync("./input.txt");
-    res.end(stream);
+    async function readFile() {
+      try {
+        const data = await fs.readFile("input.txt", "utf8");
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.write(data);
+        res.end();
+      } catch (error) {
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.write(
+          JSON.stringify({ message: "Error happened during reading file!" })
+        );
+        res.end();
+      }
+    }
+    readFile();
   }
+  
 });
 
-
-server.listen(3000, function() {
-    console.log("Server is running on port 3000");
-})
+server.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
